961e5291228a388b73c0a5742eb19b4e451a0092
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-11-21 14:22:00 -0300

    lesson 3

---
+++ README.md
@@ -0,0 +1,10 @@
+# Curso Gratuito de Java: Url Shortener
+
+> RocketSeat
+
+## Tecnologias
+
+- Java 21
+- AWS Lambda
+- AWS S3
+- AWS API Gateway
---
+++ src/main/java/com/rocketseat/hioktec/redirectUrlShortner/UrlData.java
@@ -0,0 +1,18 @@
+package com.rocketseat.hioktec.redirectUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+
+}
+
